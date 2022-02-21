49196293a85ae03743c3bc57df421a6582c1c0a8
DhananjayKadlag <dckadlag22@gmail.com>
2022-02-21 14:05:20 +0530

    "HI THIS IS MY FIRST COMMIT"

---
+++ src/com/java/aspire/Sample.java
@@ -0,0 +1,8 @@
+package com.java.aspire;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hellow Word");
+    }
+}
